d2e4a6f7fba8587eaac12e8a772b10518cf70919
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-11-07 23:49:18 +0000

    added controller code for new and create as well as user model validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,37 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-  end
+    @user = User.new(user_params)
+    @user.email.downcase!
 
-  def update
-  end
-
-  def edit
+    if @user.save
+      flash[:notice] = "Account created successfully!"
+      redirect_to root_path
+    else
+      flash[:notice] = "Account coulr not be created! - " + @user.save.errors.messages
+    end
   end
 
   def destroy
+    redirect_to root_path
   end
 
   def show
+    redirect_to root_path
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :last_name,
+      :email,
+      :password,
+    )
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  has_secure_passwords
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
 end
